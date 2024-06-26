9e06db2e4b2fd73eb35c37dba2b27b8ecffeeae4
Denis <129361495+bezymel@users.noreply.github.com>
2024-06-26 22:00:44 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,29 +1,17 @@
 data "template_file" "ansible_inventory" {
   template = <<EOT
 [webservers]
-${join("\n", var.webservers)}
+${join("\n", yandex_compute_instance.web.*.name)}
 
 [databases]
-${join("\n", var.databases)}
+${join("\n", yandex_compute_instance.db.*.name)}
 
 [storages]
-${join("\n", var.storages)}
+${join("\n", yandex_compute_instance.storage.*.name)}
 EOT
 }
 
 resource "local_file" "ansible_inventory" {
   content  = data.template_file.ansible_inventory.rendered
-  filename = "${path.module}/hosts"
-}
-
-variable "webservers" {
-  default = ["web1", "web2"]
-}
-
-variable "databases" {
-  default = ["db1", "db2"]
-}
-
-variable "storages" {
-  default = ["storage1", "storage2"]
+  filename = "${path.module}/ansible_inventory"
 }
