5ea23398746c05a13a4535b488a8b7b998a77506
Denis <129361495+bezymel@users.noreply.github.com>
2024-07-01 21:27:24 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,17 +1,29 @@
-data "template_file" "ansible_inventory" {
-  template = <<EOT
-[webservers]
-${join("\n", yandex_compute_instance.web.*.name)}
+resource "local_file" "hosts_for_ansible" {
+  content = <<-EOT
+  %{if length(yandex_compute_instance.web) > 0}
+  [webservers]
+  %{for i in yandex_compute_instance.web}
+  ${i.name} ansible_host=${i.network_interface.0.nat_ip_address} fqdn=${i.name}
+  %{endfor}
+  %{endif}
 
-[databases]
-${join("\n", yandex_compute_instance.db.*.name)}
+  %{if length(yandex_compute_instance.database) > 0}
+  [databases]
+  %{for i in yandex_compute_instance.database}
+  ${i.value.name} ansible_host=${i.value.network_interface.0.nat_ip_address} fqdn=${i.value.name}
+  %{endfor}
+  %{endif}
 
-[storages]
-${join("\n", yandex_compute_instance.storage.*.name)}
-EOT
+  %{if length(yandex_compute_instance.storage) > 0}
+  [storage]
+  %{for i in yandex_compute_instance.storage}
+  ${i.name} ansible_host=${i.network_interface.0.nat_ip_address} fqdn=${i.name}
+  %{endfor}
+  %{endif}
+  EOT
+  filename = "${abspath(path.module)}/hosts"
 }
 
-resource "local_file" "ansible_inventory" {
-  content  = data.template_file.ansible_inventory.rendered
-  filename = "${path.module}/ansible_inventory"
+output "ansible_inventory" {
+  value = local_file.hosts_for_ansible.content
 }
