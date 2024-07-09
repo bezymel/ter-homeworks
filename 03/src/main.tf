ea7a37e502eb1afd8c33105d7fccd201a499b1a4
Denis <129361495+bezymel@users.noreply.github.com>
2024-07-09 22:40:56 +0300

    Update main.tf

--- 03/src/main.tf
+++ 03/src/main.tf
@@ -8,6 +8,10 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+data "yandex_compute_image" "ubuntu-2004-lts" {
+  family = "ubuntu-2004-lts"
+}
+
 locals {
   ssh_key = ""
 }
